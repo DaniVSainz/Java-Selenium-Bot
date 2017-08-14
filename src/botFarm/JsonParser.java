c28a717b64fc57fa2bf41256a2c1b75864580468
Daniel V Sainz <danivsainz@gmail.com>
2017-08-14 19:54:13 -0400

    More work

--- src/botFarm/JsonParser.java
+++ src/botFarm/JsonParser.java
@@ -2,6 +2,7 @@ package botFarm;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.util.Arrays;
 
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
@@ -19,20 +20,21 @@ public class JsonParser {
 
 //	Parses the string return from getRequest and assigns the values to a Array of Strings to be passed back
 //	to our bot
-	public static JsonValue parseNewUser(String jsonString) {
+	public static String[] parseNewUser(String jsonString) {
 		JsonArray array = Json.parse(jsonString).asObject().get("results").asArray();
-		System.out.println(array);
-		System.out.println(array.get(0).asObject().get("name").asObject().get("first"));
 		myUser[0] = array.get(0).asObject().get("gender").toString();
 		myUser[1] = array.get(0).asObject().get("name").asObject().get("first").toString();
 		myUser[2] = array.get(0).asObject().get("name").asObject().get("last").toString();
+		myUser[3] = array.get(0).asObject().get("location").asObject().get("city").toString();
+		myUser[4] = array.get(0).asObject().get("location").asObject().get("state").toString();
 		myUser[5] = array.get(0).asObject().get("picture").asObject().get("large").toString();
-		return array.get(0);
+		return myUser;
 	}
 	
+//	Mainly for testing purposes you can ignore or use to test this class.
 	public static void main(String[] args) throws Exception {
 		String getUser = "https://randomuser.me/api/?inc=gender,name,nat,location,picture";
 		GetRequest requestor = new GetRequest();
-		parseNewUser(requestor.get(getUser));
+		System.out.println(Arrays.toString(parseNewUser(requestor.get(getUser))));
 	}
 }
